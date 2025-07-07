db0711ed0bc3dd01d1742b39795390a253674336
vinay <mhvinay5@gmail.com>
2025-07-07 22:32:18 +0530

    ok

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,22 @@
 import express from "express";
-
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 const app = express()
-
-app.listen(5001,()=>{
-    console.log("Server is running on port 5001")
+app.get("/",(req,res)=>{
+    res.send("Hello World")
 })
+
+const startServer = async () => {
+    try{
+        await connectDB()
+        app.listen(ENV.PORT,()=>{
+            console.log(`Server is running on port ${ENV.PORT}`)
+        })
+    }
+    catch(e){
+        console.log("Error starting server",e)
+        process.exit(1)
+    }
+}
+
+startServer()
